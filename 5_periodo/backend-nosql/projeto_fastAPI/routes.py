f626d8726d4532e7a6ab55f48caa8bcbdef51249
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2026-03-24 20:37:29 -0300

    feat: crud de carros

--- 5_periodo/backend-nosql/projeto_fastAPI/routes.py
+++ 5_periodo/backend-nosql/projeto_fastAPI/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from database import users_collection
-from schemas import User
+from database import users_collection, carros_collection
+from schemas import User, Carro
 
 router = APIRouter()
 
@@ -60,3 +60,60 @@ def update_id(user_id: str, data: dict):
         return {"error": "user not found"}
     
     return {"message": "update successful"}
+
+@router.get("/carros")
+def list_carros():
+    carros = []
+
+    for carro in carros_collection.find():
+        carro["_id"] = str(carro["_id"])
+        carros.append(carro)
+
+    return carros
+
+@router.post("/carros")
+def create_carro(carro: Carro):
+
+    carro_dict = carro.model_dump()
+    result = carros_collection.insert_one(carro_dict)
+    
+    return {
+        "message": "carro criado",
+        "id": str(result.inserted_id)
+    }
+
+@router.get("/carros/{carro_id}")
+def get_carro(carro_id: str):
+    from bson import ObjectId
+
+    carro = carros_collection.find_one({"_id": ObjectId(carro_id)})
+
+    if carro:
+        carro["_id"] = str(carro["_id"])
+        return carro
+    return {"error": "carro não encontrado"}
+
+@router.delete("/carros/{carro_id}")
+def delete_carro(carro_id: str):
+    from bson import ObjectId
+
+    result = carros_collection.delete_one({"_id": ObjectId(carro_id)})
+
+    if result.deleted_count == 0:
+        return {"error": "carro não encontrado"}
+    
+    return {"message": "carro deletado com sucesso"}
+
+@router.patch("/carros/{carro_id}")
+def update_carro(carro_id: str, data: dict):
+    from bson import ObjectId
+
+    result = carros_collection.update_one(
+        {"_id": ObjectId(carro_id)},
+        {"$set": data}
+    )
+
+    if result.matched_count == 0:
+        return {"error": "carro não encontrado"}
+    
+    return {"message": "atualização realizada com sucesso"}
